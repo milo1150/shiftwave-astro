0e98953fd769f4fa7d2a2238340f5144280731cd
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-06 17:26:36 +0700

    fix: mobile view

--- src/components/user/reviews/UserReviewComponent.tsx
+++ src/components/user/reviews/UserReviewComponent.tsx
@@ -1,5 +1,5 @@
 import { convertLangEnum, useTranslations } from '@src/i18n/utils'
-import { Button, Card, ConfigProvider, Input, Rate } from 'antd'
+import { Button, Card, Col, ConfigProvider, Input, Rate, Row } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
 import React, { useEffect, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
@@ -65,71 +65,78 @@ const UserReviewComponent: React.FC<Props> = ({ lang, branchId }) => {
 
   if (isRateLimit) {
     return (
-      <div className="h-full content-center place-items-center">
+      <div className="h-full content-center place-items-center font-bold">
         <CompleteReview lang={lang} />
       </div>
     )
   }
 
   return (
-    <div className="flex text-center justify-center p-9 ">
-      <Card className="w-1/3 shadow-lg rounded-xl border-none">
-        <p className="text-2xl font-bold">{t('rating.title')}</p>
+    <Row className="justify-center h-full">
+      <Col
+        xl={10}
+        lg={18}
+        xs={24}
+        className="flex text-center justify-center p-2"
+      >
+        <div className="w-full h-full flex flex-col shadow-lg rounded-xl border-none p-5">
+          <p className="text-xl font-bold">{t('rating.title')}</p>
 
-        <p className="text-md pt-2">{t('rating.subtitle')}</p>
+          <p className="text-sm pt-2">{t('rating.subtitle')}</p>
 
-        <div className="pb-6 pt-3">
-          <Rate
-            onChange={setReviewScore}
-            value={reviewScore}
-            className="text-4xl"
-          />
-        </div>
-
-        <div>
-          <ConfigProvider
-            theme={{
-              token: {
-                fontFamily: 'kanit',
-              },
-            }}
-          >
-            <TextArea
-              rows={1}
-              maxLength={250}
-              placeholder={t('rating.placeholder')}
-              style={{ height: 120, resize: 'none' }}
-              className="rounded-xl border-slate-200"
-              value={remark}
-              onChange={(e) => setRemark(e.target.value)}
+          <div className="pb-6 pt-3">
+            <Rate
+              onChange={setReviewScore}
+              value={reviewScore}
+              className="text-4xl"
             />
-          </ConfigProvider>
-        </div>
+          </div>
 
-        <div className="pt-1">
-          <ConfigProvider
-            theme={{
-              token: {
-                colorPrimary: '#38bdf8',
-                colorBorder: 'none',
-                fontFamily: 'kanit',
-              },
-            }}
-          >
-            <Button
-              style={{ height: 45 }}
-              className="w-full rounded-xl"
-              type="primary"
-              disabled={reviewScore === 0 && typeof branchId === 'number'}
-              loading={createReviewMutation.status === 'pending'}
-              onClick={() => onClickCreate()}
+          <div>
+            <ConfigProvider
+              theme={{
+                token: {
+                  fontFamily: 'kanit',
+                },
+              }}
             >
-              {t('rating.submit')}
-            </Button>
-          </ConfigProvider>
+              <TextArea
+                rows={1}
+                maxLength={250}
+                placeholder={t('rating.placeholder')}
+                style={{ height: 120, resize: 'none' }}
+                className="rounded-xl border-slate-200"
+                value={remark}
+                onChange={(e) => setRemark(e.target.value)}
+              />
+            </ConfigProvider>
+          </div>
+
+          <div className="pt-3">
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: '#38bdf8',
+                  colorBorder: 'none',
+                  fontFamily: 'kanit',
+                },
+              }}
+            >
+              <Button
+                style={{ height: 45 }}
+                className="w-full rounded-xl"
+                type="primary"
+                disabled={reviewScore === 0 && typeof branchId === 'number'}
+                loading={createReviewMutation.status === 'pending'}
+                onClick={() => onClickCreate()}
+              >
+                {t('rating.submit')}
+              </Button>
+            </ConfigProvider>
+          </div>
         </div>
-      </Card>
-    </div>
+      </Col>
+    </Row>
   )
 }
 
